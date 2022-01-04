f6c3061e00d588e9783d1d8c392f55e43b3b8058
wil <Workoutwwill@gmail.com>
2022-01-04 02:42:25 -0500

    3rd  commit

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
           <div className='left'>
             <h1 className=' text-3xl sm:text-4xl  anim '>Hello I'm <span className='text-blue-500 font-bold whitespace-nowrap'>Wil</span>,</h1>
             <h1 className=' text-3xl sm:text-4xl'>Full stack developer</h1>
+            <p className='text-slate-400 italic mt-4'>&lt;p&gt;React Fullstack Developer   &lt;/p&gt; </p>
             <div className="mt-10 flex  justify-center gap-2">
             <a href="" className=" whitespace-nowrap border-solid border-white border-2  py-2 px-4 rounded flex justify-center">Contact me</a>
             <a href="" className="whitespace-nowrap bg-blue-500 text-white   py-2 px-4 rounded flex justify-center">Portfolio</a>
